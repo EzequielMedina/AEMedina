abb2cd15d136364b686a6448407fe6cafec878a3
EzequielMedina <84730@sistemas.frc.utn.edu.ar>
2021-08-16 21:46:44 -0300

    nuevo

---
+++ src/monckData.js
@@ -0,0 +1,16 @@
+
+import CalculadoraJS from "./img/CalculadoraJS.jpg";
+//import calcu from "./Calculadora 2.0/index.html";
+const mockData = [
+
+    {
+        title: "Calculadora JS",
+        image: CalculadoraJS,
+        repositorio: "https://github.com/EzequielMedina/MiPortafolio/blob/gh-pages/Calculadora%202.0/index.html",
+        video: "",
+    }
+
+
+]
+
+export default mockData;
