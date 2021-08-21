ffd717bbe7f4e5b7d4aa9ce3e7def012118cccf5
EzequielMedina <84730@sistemas.frc.utn.edu.ar>
2021-08-21 15:48:55 -0300

    21/08/21 calculadora

--- src/monckData.js
+++ src/monckData.js
@@ -6,7 +6,7 @@ const mockData = [
     {
         title: "Calculadora JS",
         image: CalculadoraJS,
-        repositorio: "https://github.com/EzequielMedina/MiPortafolio/blob/gh-pages/Calculadora%202.0/index.html",
+        repositorio: "https://github.com/EzequielMedina/AEMedina/blob/master/src/Calculadora%202.0/index.html",
         video: "",
     }
 
